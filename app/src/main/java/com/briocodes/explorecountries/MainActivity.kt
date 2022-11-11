4df26d98b636d5c86229a4ca2aa522805fcab21e
BRIGHT <briocodes@gmail.com>
2022-11-11 23:52:43 +0100

    Concluded on the short-term language and filter section implementation

--- app/src/main/java/com/briocodes/explorecountries/MainActivity.kt
+++ app/src/main/java/com/briocodes/explorecountries/MainActivity.kt
@@ -1,23 +1,64 @@
 package com.briocodes.explorecountries
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.view.Window
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var langButton: Button
+    private lateinit var filterButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
-
         langButton = findViewById(R.id.language_btn)
-        langButton.setOnClickListener(View.OnClickListener {  v ->
-            val intent = Intent(this, DetailsActivity::class.java)
-            startActivity(intent)
+        filterButton = findViewById(R.id.filter_btn)
+
+        langButton.setOnClickListener(View.OnClickListener { v ->
+            showSheetDialog()
         })
+
+        filterButton.setOnClickListener(View.OnClickListener { v ->
+            showFilterDialog()
+        })
+
+    }
+
+    private fun showFilterDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottom_filter_layout)
+
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
+    }
+
+    private fun showSheetDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottom_sheet_layout)
+
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
     }
 }
