201577e1a7baac44e5d55018a1bf1462e1bfc616
BRIGHT <briocodes@gmail.com>
2022-11-13 10:30:23 +0100

    Loaded the flag images of each of the countries with Glide

--- app/src/main/java/com/briocodes/explorecountries/adapters/CountryAdapter.kt
+++ app/src/main/java/com/briocodes/explorecountries/adapters/CountryAdapter.kt
@@ -4,17 +4,20 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.briocodes.explorecountries.R
 import com.briocodes.explorecountries.dataclass.CountryDataItem
+import com.briocodes.explorecountries.dataclass.subdataclass.Flags
+import com.bumptech.glide.Glide
 
 class CountryAdapter (private val context: Context, private val countryList:List<CountryDataItem>) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>(){
 
     inner class CountryViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
         val coutryName: TextView = itemView.findViewById(R.id.country_name)
         val countryCapital: TextView = itemView.findViewById(R.id.country_capital)
-        //val countryFlag: ImageView = itemView.findViewById(R.id.country_flag)
+        val countryFlag: ImageView = itemView.findViewById(R.id.country_flag)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
@@ -25,7 +28,8 @@ class CountryAdapter (private val context: Context, private val countryList:List
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
         holder.coutryName.text = countryList[position].name.common
         holder.countryCapital.text = countryList[position].capital.toString()
-        //holder.countryFlag.setImageResource(countryList[position].flag)
+        Glide.with(holder.countryFlag).load(countryList[position].flags.png).into(holder.countryFlag)
+
     }
 
     override fun getItemCount(): Int {
